747fee9922e055534d1285ae5ac346728f1accfa
Omaru Jawara <omarito.jawara@gmail.com>
2021-09-26 19:34:14 -0400

    Create tweet design

--- Twitter/LoginViewController.swift
+++ Twitter/LoginViewController.swift
@@ -10,15 +10,24 @@ import UIKit
 
 class LoginViewController: UIViewController {
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        if UserDefaults.standard.bool(forKey: "userLoggedIn") == true {
+            self.performSegue(withIdentifier: "loginToHome", sender: self)
+        }
     }
     
     @IBAction func onLoginButton(_ sender: Any) {
         let twitterURL = "https://api.twitter.com/oauth/request_token"
         TwitterAPICaller.client?.login(url: twitterURL, success: {
+            
+            UserDefaults.standard.set(true, forKey: "userLoggedIn")
+            
             self.performSegue(withIdentifier: "loginToHome", sender: self)
         }, failure: { Error in
             print("Login failed!")
